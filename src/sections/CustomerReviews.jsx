8ac4a03c2e29268d664f1175d65f8aec1355d0fb
Rokas <rookas.rudzenskas@gmail.com>
2023-08-19 15:14:31 +0300

    🔧 fix(CustomerReviews.jsx): rename 'review' prop to 'feedback' in ReviewCard component for better clarity and consistency

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -17,11 +17,11 @@ const CustomerReviews = () => {
       <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
         {reviews.map((review, index) => (
             <ReviewCard
-              key={index}
-              imgURL={review.imgURL}
-              customerName={review.customerName}
-              rating={review.rating}
-              review={review.feedback}
+                key={index}
+                imgURL={review.imgURL}
+                customerName={review.customerName}
+                rating={review.rating}
+                feedback={review.feedback}
             />
         ))}
       </div>
