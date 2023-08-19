a3421a691020114066a083f11e2f563d05839549
Rokas <rookas.rudzenskas@gmail.com>
2023-08-19 15:18:09 +0300

    🎨 style(Subscribe.jsx): add Sign Up button to the Subscribe section for better user experience

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -12,6 +12,9 @@ const Subscribe = () => {
       </h3>
       <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
         <input type="text" placeholder="subscribe@nike.com" className="input" />
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
+          <Button label="Sign Up" fullWidth />
+        </div>
       </div>
     </section>
   );
