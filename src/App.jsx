cc13a081721a2eabdf1079da6395407c0d5053b0
Rokas <rookas.rudzenskas@gmail.com>
2023-08-19 09:27:09 +0300

    🔥 refactor(App.jsx): remove commented out code to improve code readability and maintainability

--- src/App.jsx
+++ src/App.jsx
@@ -14,30 +14,30 @@ const App = () => {
   return (
     <main className='relative'>
       <Nav />
-      {/*<section className='xl:padding-l wide:padding-r padding-b'>*/}
+      <section className='xl:padding-l wide:padding-r padding-b'>
       {/*  <Hero />*/}
-      {/*</section>*/}
-      {/*<section className='padding'>*/}
+      </section>
+      <section className='padding'>
       {/*  <PopularProducts />*/}
-      {/*</section>*/}
-      {/*<section className='padding'>*/}
+      </section>
+      <section className='padding'>
       {/*  <SuperQuality />*/}
-      {/*</section>*/}
-      {/*<section className='padding-x py-10'>*/}
+      </section>
+      <section className='padding-x py-10'>
       {/*  <Services />*/}
-      {/*</section>*/}
-      {/*<section className='padding'>*/}
+      </section>
+      <section className='padding'>
       {/*  <SpecialOffer />*/}
-      {/*</section>*/}
-      {/*<section className='bg-pale-blue padding'>*/}
+      </section>
+      <section className='bg-pale-blue padding'>
       {/*  <CustomerReviews />*/}
-      {/*</section>*/}
-      {/*<section className='padding-x sm:py-32 py-16 w-full'>*/}
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
       {/*  <Subscribe />*/}
-      {/*</section>*/}
-      {/*<section className=' bg-black padding-x padding-t pb-8'>*/}
+      </section>
+      <section className=' bg-black padding-x padding-t pb-8'>
       {/*  <Footer />*/}
-      {/*</section>*/}
+      </section>
     </main>
   );
 };
